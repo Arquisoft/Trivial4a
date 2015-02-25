36db23b97a3daf05adfc5f13fe603d13bfbfc57c
Jorge Sierra Miranda <jk8lp8@hotmail.com>
2015-02-25 20:25:25 +0100

    Tratamiento de argumentos en el main
    Tratamiento de los argumentos en el main para que la clase Extractor
    reciba por parámetros la ruta del fichero a transformar (absoluta o
    relativa) ; incluyendo tratamiento de errores.
    
    Leer TODO en los ficheros fuentes.

--- extract/src/main/java/es/uniovi/asw/trivial/Extractor.java
+++ extract/src/main/java/es/uniovi/asw/trivial/Extractor.java
@@ -1,19 +1,49 @@
 package es.uniovi.asw.trivial;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.imageio.stream.FileImageInputStream;
+
 public class Extractor {
-	
-	public void usage() {
-		System.out.println("Wellcome to Trivial Extractor");
-	}
-	public int run(String[] args) {
-		if (args.length == 0) {
-			usage();
-			return 0;
-		}
-		return -1 ;
-	}
 	
 	public static void main(String[] args) {
         new Extractor().run(args);
     }	
+	
+	public int run(String[] args) {
+		if (args.length == 0) {
+			mostrarErrorNoFargumentos();
+			return -1;
+		}
+		try {
+			
+			FileImageInputStream inputStream = new FileImageInputStream(new File(args[0]));
+			
+		} catch (FileNotFoundException e) {
+			System.err.println("No se ha encontrado el archivo.");
+		} catch (IOException e) {
+			System.err.println("No se ha podido cargar el archivo.");
+			return -1;
+		}
+		/*
+		 * TODO 
+		 * El parser creado con yacc y jflex debe invocarse desde aqui.
+		 * Deberá verificar el fichero y procesarlo para que devuelva una estructura de datos que contenta las preguntas.
+		 * Esta estructura de datos se recorrera despues para generar el json.
+		 * Para pasar la lista de objetos a Json intentaremos usar gson.
+		 * http://search.maven.org/#artifactdetails%7Ccom.google.code.gson%7Cgson%7C2.3.1%7Cjar
+		 */
+
+		System.out.println("To'bien.");
+		return 0 ;
+	}
+	
+	private void mostrarErrorNoFargumentos() {
+		System.err.println("No se han espeficicado argumentos.");
+		
+	}
+
+
 }
---
+++ extract/src/main/java/es/uniovi/asw/trivial/pregunta/Pregunta.java
@@ -0,0 +1,13 @@
+package es.uniovi.asw.trivial.pregunta;
+
+public class Pregunta {
+	
+	/*
+	 * TODO
+	 * Rellenar la clase con los atributos que usará en el JSON 
+	 * 
+	 * Revisar el siguiente enlace:
+	 * http://www.mkyong.com/java/how-do-convert-java-object-to-from-json-format-gson-api/
+	 */
+
+}
