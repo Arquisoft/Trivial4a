250189768b8a30c2869065466238dd9a33be2dc0
DavidSFdez <davidsfdez@gmail.com>
2015-03-10 18:16:41 +0100

    Actualizacion clases juego y tablero

--- Trivial4aCore/src/main/java/es/uniovi/asw/trivial/Juego.java
+++ Trivial4aCore/src/main/java/es/uniovi/asw/trivial/Juego.java
@@ -1,13 +1,39 @@
 package es.uniovi.asw.trivial;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Juego {
 	
 	private Tablero tablero;
 	private List<Jugador> jugadores;
-	
+	private List<String> categorias;
 	private List<Categoria> preguntas;
+	private Jugador jugadorActivo;
 	
+	public Juego(int tamañoTablero, List<Jugador> jugadores,List<Categoria> preguntas)
+	{
+		
+		this.jugadores=jugadores;
+		this.preguntas=preguntas;
+		categorias= new ArrayList<String>();
+		
+		for(Categoria c: preguntas)
+			if(!categorias.contains(c.getCategoria()))
+				categorias.add(c.getCategoria());
+		
+		jugadorActivo = jugadores.get(0);
+		
+		tablero = new Tablero(tamañoTablero, categorias, jugadores);
+	}
+	
+	public boolean responderPregunta()
+	{
+		Categoria categoriaActual=null;
+		categoriaActual=tablero.getCategoria(jugadorActivo);
+		
+		
+		return false;
+	}
 
 }
--- Trivial4aCore/src/main/java/es/uniovi/asw/trivial/Tablero.java
+++ Trivial4aCore/src/main/java/es/uniovi/asw/trivial/Tablero.java
@@ -1,7 +1,39 @@
 package es.uniovi.asw.trivial;
 
+import java.util.List;
+
 public class Tablero {
 	
 	//TODO a rellenar
+	private int tamaño;
+	private List<String> categorias;
+	private List<Jugador> jugadores;
+	private String[] categoriaQuesito;
+	
+	public Tablero(int tamaño,List<String> categorias,List<Jugador> jugadores) {
+		this.categorias=categorias;
+		this.tamaño = tamaño;
+		this.jugadores=jugadores;
+		categoriaQuesito = new String[tamaño];
+		establecerCasillasQuesito();
+	}
+	
+	public Categoria getCategoria(Jugador jugadorActivo) {
+		
+		return getCategoriaCasilla(jugadorActivo.getPosicion());
+	}
+	
+	private Categoria getCategoriaCasilla(int posicion)
+	{
+		// TODO Elegir categoria en funcion de la casilla en la que se encuentre
+		Categoria c = null;
+		
+		return c;
+	}
+	private void establecerCasillasQuesito()
+	{
+		//TODO en cuncion del tamaño del tablero y el numero de categorias pone en cuales habra quesito
+		
+	}
 
 }
