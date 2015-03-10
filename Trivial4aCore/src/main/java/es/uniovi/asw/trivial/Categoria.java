3240d9ddc9fff4811c07a8a788b939aeb94c7747
Jorge Sierra Miranda <jk8lp8@hotmail.com>
2015-03-10 17:04:27 +0100

    Creada clase categoría

---
+++ Trivial4aCore/src/main/java/es/uniovi/asw/trivial/Categoria.java
@@ -0,0 +1,17 @@
+package es.uniovi.asw.trivial;
+
+public class Categoria {
+	
+	private String categoria;
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+
+}
