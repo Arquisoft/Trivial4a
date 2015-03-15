b56c282b939c88fdd924239aa2709c9db512fc0e
Jorge Sierra Miranda <jk8lp8@hotmail.com>
2015-03-15 20:51:37 +0100

    Salida de ficheros
    * Se posibilita el sacar ficheros json con la salida del extract.
    * Se reorganiza los paquetes para adaptarse mejor al modelo MVC.

---
+++ Extract4a/src/main/java/es/uniovi/asw/trivial/extractor/view/Main.java
@@ -0,0 +1,20 @@
+package es.uniovi.asw.trivial.extractor.view;
+
+import java.io.FileNotFoundException;
+import java.net.UnknownHostException;
+
+import es.uniovi.asw.trivial.extractor.parser.Extractor;
+
+public class Main {
+	
+	public static void main(String[] args){
+		try {
+			new Extractor().run(args);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
